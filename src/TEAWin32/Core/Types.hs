ff4aa1d7ee10eea201bb3ec80ccd20667b2aea45
Toma Sasaki <netst915@gmail.com>
2026-03-01 16:38:47 +0000

    a

--- src/TEAWin32/Core/Types.hs
+++ src/TEAWin32/Core/Types.hs
@@ -1,11 +1,31 @@
+{-# LANGUAGE ExistentialQuantification #-}
+
 module TEAWin32.Core.Types
     ( UniqueId (..)
+    , Colour (..)
     , TEAWin32Settings (..)
     , ScalableValue (..)
     , RawValue (..)
+    , WindowStyle (..)
+    , IconType (..)
+    , Icon (..)
+    , Cursor (..)
+    , Font (..)
+    , ComponentProperty (..)
+    , ComponentTitle (..)
+    , ComponentSize (..)
+    , ComponentPosition (..)
+    , ComponentFont (..)
+    , WindowProperty (..)
+    , WindowIcon (..)
+    , WindowCursor (..)
+    , WindowBackgroundColour (..)
+    , ButtonProperty (..)
     , defaultTEAWin32Settings
     ) where
 
+import           Data.Data (Typeable)
+import           Data.Text (Text)
 import           Foreign   (Storable (..), fillBytes)
 import           Foreign.C (CInt)
 
@@ -22,6 +42,8 @@ instance Storable UniqueId where
         fillBytes ptr 0 (sizeOf (undefined :: UniqueId)) >>
             pokeByteOff ptr 0 uniqueId
 
+data Colour = RGB Int Int Int deriving (Show, Eq)
+
 newtype TEAWin32Settings = TEAWin32Settings
     { useVisualStyles :: Bool
     }
@@ -33,7 +55,7 @@ defaultTEAWin32Settings = TEAWin32Settings
 
 data ScalableValue = ScalableValue Double
                    | RawValue      Double
-                   deriving Eq
+                   deriving (Show, Eq, Ord)
 
 class RawValue a where
     raw :: a -> ScalableValue
@@ -77,4 +99,91 @@ instance Fractional ScalableValue where
     (ScalableValue a) / (RawValue b)      = ScalableValue (a / b)
     (RawValue a)      / (ScalableValue b) = RawValue      (a / b)
 
+data WindowStyle = WindowStyleBorderless
+                 | WindowStyleNormal
+                 | WindowStyleBorderlessChild
+                 | WindowStyleNormalChild
+                 deriving (Show, Eq, Ord)
 
+data IconType = ResourceIcon | StockIcon deriving Eq
+
+data Icon = IconDocNoAssoc | IconDocAssoc | IconApplication | IconFolder | IconFolderOpen | IconDrive525 | IconDrive35 | IconDriveRemove
+          | IconDriveFixed | IconDriveNet | IconDriveNetDisabled | IconDriveCD | IconDriveRAM | IconWorld | IconServer | IconPrinter
+          | IconMyNetwork | IconFind | IconHelp | IconShare | IconLink | IconSlowFile | IconRecycler | IconRecyclerFull | IconMediaCDAudio
+          | IconLock | IconAutoList | IconPrinterNet | IconServerShare | IconPrinterFax | IconPrinterFaxNet | IconPrinterFile | IconStack
+          | IconMediaSVCD | IconStuffedFolder | IconDriveUnknown | IconDriveDVD | IconMediaDVD | IconMediaDVDRAM | IconMediaDVDRW
+          | IconMediaDVDR | IconMediaDVDROM | IconMediaCDAudioPlus | IconMediaCDRW | IconMediaCDR | IconMediaCDBurn | IconMediaBlankCD
+          | IconMediaCDROM | IconAudioFiles | IconImageFiles | IconVideoFiles | IconMixedFiles | IconFolderBack | IconFolderFront
+          | IconShield | IconWarning | IconInfo | IconError | IconKey | IconSoftware | IconRename | IconDelete | IconMediaAudioDVD
+          | IconMediaMovieDVD | IconMediaEnhancedCD | IconMediaEnhancedDVD | IconMediaHDDVD | IconMediaBluray | IconMediaVCD
+          | IconMediaDVDPlusR | IconMediaDVDPlusRW | IconDesktopPC | IconMobilePC | IconUsers | IconMediaSmartMedia | IconMediaCompactFlash
+          | IconDeviceCellPhone | IconDeviceCamera | IconDeviceVideoCamera | IconDeviceAudioPlayer | IconNetworkConnect | IconInternet
+          | IconZipFile | IconSettings | IconDriveHDDVD | IconDriveBD | IconMediaHDDVDROM | IconMediaHDDVDR | IconMediaHDDVDRAM | IconMediaBDROM
+          | IconMediaBDR | IconMediaBDRE | IconClusteredDrive | IconFromResourceFile Int
+          deriving (Show, Eq, Ord)
+
+data Cursor = CursorArrow
+            | CursorIBeam
+            | CursorWait
+            | CursorCross
+            | CursorUparrow
+            | CursorSizeNWSE
+            | CursorSizeNESW
+            | CursorSizeWE
+            | CursorSizeNS
+            deriving (Show, Eq, Ord)
+
+data Font = Font
+    { fontName    :: Text
+    , fontSize    :: ScalableValue
+    , isItalic    :: Bool
+    , isUnderline :: Bool
+    , isStrikeOut :: Bool
+    } deriving (Show, Eq, Ord)
+
+data ComponentProperty = forall a. (Typeable a, Show a, IsComponentProperty a) => ComponentProperty a
+
+class IsComponentProperty a
+
+newtype ComponentTitle    = ComponentTitle    Text                           deriving (Show, Eq)
+newtype ComponentSize     = ComponentSize     (ScalableValue, ScalableValue) deriving (Show, Eq)
+newtype ComponentPosition = ComponentPosition (ScalableValue, ScalableValue) deriving (Show, Eq)
+newtype ComponentFont     = ComponentFont     Font                           deriving (Show, Eq)
+
+instance IsComponentProperty ComponentTitle
+instance IsComponentProperty ComponentSize
+instance IsComponentProperty ComponentPosition
+instance IsComponentProperty ComponentFont
+
+data WindowProperty = forall a. (Typeable a, Show a, IsComponentProperty a, IsWindowProperty a) => WindowProperty a
+
+instance IsComponentProperty WindowProperty
+
+class IsWindowProperty a
+
+newtype WindowIcon             = WindowIcon             Icon   deriving (Show, Eq)
+newtype WindowCursor           = WindowCursor           Cursor deriving (Show, Eq)
+newtype WindowBackgroundColour = WindowBackgroundColour Colour deriving (Show, Eq)
+
+instance IsComponentProperty WindowIcon
+instance IsComponentProperty WindowCursor
+instance IsComponentProperty WindowBackgroundColour
+
+instance IsWindowProperty WindowIcon
+instance IsWindowProperty WindowCursor
+instance IsWindowProperty WindowBackgroundColour
+instance IsWindowProperty ComponentTitle
+instance IsWindowProperty ComponentSize
+instance IsWindowProperty ComponentPosition
+instance IsWindowProperty ComponentFont
+
+data ButtonProperty = forall a. (Typeable a, Show a, IsComponentProperty a, IsButtonProperty a) => ButtonProperty a
+
+instance IsComponentProperty ButtonProperty
+
+class IsButtonProperty a
+
+instance IsButtonProperty ComponentTitle
+instance IsButtonProperty ComponentSize
+instance IsButtonProperty ComponentPosition
+instance IsButtonProperty ComponentFont
